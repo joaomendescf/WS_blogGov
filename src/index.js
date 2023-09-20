b079c0cb70aac8238cfd629845c9682c43b2b4c5
joaomendescf <joaomendes.ifce@gmail.com>
2023-09-20 00:48:43 -0300

    ws link da pagina pai

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-// const readline = require('readline');
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-  
- 
-axios.get('https://www.gov.br/pt-br/noticias/financas-impostos-e-gestao-publica')
+const urlPai = 'https://www.gov.br/pt-br/noticias/financas-impostos-e-gestao-publica'
+
+axios.get(urlPai)
 .then(resp=>{
 
     let paginaHtml = resp.data
     let $ = cheerio.load(paginaHtml)
 
-    let colunas = []
+    let dados = []
     $('[class="summary url"]').each((i,e)=>{
 
-        let titulo = $(e).text()
+        // let titulo = $(e).text()
         let link = $(e).attr('href')
-
-        let coluna = {titulo, link}
-        colunas.push(coluna)
+        dados.push(link)
     })
 
-    // console.log(colunas)
+    console.log(dados)
 
-    for(let coluna in colunas){
-        // console.log(coluna['link'])
-        // rl.question('===================================');
-        coletarDataPub(coluna['link'])
-    }
+    // for(let coluna in colunas){
+    //     coletarDataPub(coluna['link'])
+    // }
 })
 
 function coletarDataPub(link){    
